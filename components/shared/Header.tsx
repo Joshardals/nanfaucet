68097fecdef0717bdb9e817e8538774a48bc5040
Joshardals <joshuabamidele219@gmail.com>
2024-10-24 05:02:39 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { HiMiniXMark } from "react-icons/hi2";
 import { IoIosSearch } from "react-icons/io";
+import { motion, AnimatePresence } from "framer-motion";
 import { PiGlobeBold } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxDividerVertical } from "react-icons/rx";
 import { useState } from "react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="max-content p-5 text-sm font-medium">
       <div className="flex items-center justify-between md:space-x-5">
@@ -14,7 +19,10 @@ export function Header() {
           giftcards
         </span>
         <div className="flex items-center space-x-5">
-          <RxHamburgerMenu className="size-5 md:hidden cursor-pointer" />
+          <RxHamburgerMenu
+            className="size-5 md:hidden cursor-pointer"
+            onClick={toggleSidebar}
+          />
           <span className="text-accent md:hidden">logo</span>
         </div>
         <div className="ring-1 ring-primary/10 bg-primary/[0.02] text-primary/50 cursor-pointer outline-none flex-1 px-4 py-2 rounded-md hover-effects hover:text-primary flex items-center space-x-2 max-md:hidden">
@@ -33,26 +41,41 @@ export function Header() {
         </div>
       </div>
 
-      <Sidebar />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
     </header>
   );
 }
 
-function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
+function Sidebar({
+  isOpen,
+  toggleSidebar,
+}: {
+  isOpen: boolean;
+  toggleSidebar: any;
+}) {
   return (
-    <div className="bg-black/80 fixed top-0 right-0 left-0 w-full">
-      <div className="bg-white h-screen w-[75vw] p-5 font-medium">
-        <HiMiniXMark
-          className="size-6 float-right cursor-pointer text-primary/50 hover-effects hover:text-primary"
-          onClick={toggleSidebar}
-        />
-      </div>
+    <div
+      className="bg-black/80 fixed top-0 right-0 left-0 w-full"
+      onClick={toggleSidebar}
+    >
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key="sidebar" // Key helps AnimatePresence track the component
+            initial={{ x: "-100%" }} // Start off-screen
+            animate={{ x: 0 }} // Animate in
+            exit={{ x: "-100%" }} // Animate out
+            transition={{ type: "tween", duration: 0.5 }} // Animation timing
+            className="bg-white h-screen w-[75vw] p-5 font-medium"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <HiMiniXMark
+              className="size-6 float-right cursor-pointer text-primary/50 hover-effects hover:text-primary"
+              onClick={toggleSidebar}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
