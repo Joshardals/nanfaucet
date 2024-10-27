ab10ff9263abfb2fc42121f25406903cba6499db
Joshardals <joshuabamidele219@gmail.com>
2024-10-27 07:29:40 +0100

    initial commit

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,7 +1,7 @@
 import { LoginForm } from "@/components/LoginForm";
 import Image from "next/image";
 import Link from "next/link";
-import { getCurrentUser } from "@/lib/actions/auth.actions";
+// import { getCurrentUser } from "@/lib/actions/auth.actions";
 // import { redirect } from "next/navigation";
 
 export default async function page() {
