9efc5855a5238550b006a269fd5e0a1c6ee5f7d1
Joshardals <joshuabamidele219@gmail.com>
2024-10-28 05:21:16 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,31 @@ import "react-toastify/dist/ReactToastify.css";
 export const metadata: Metadata = {
   title: "Nano Faucet - Earn Free Nano Cryptocurrency Effortlessly",
   description:
-    "Join the Nano Faucet platform to earn free Nano cryptocurrency by referring friends. Claim your airdrop and be part of the growing Nano community today!",
+    "Join the Nano Faucet platform to earn free Nano cryptocurrency by referring friends and completing simple tasks. Claim your airdrop and be part of the growing Nano community today!",
+  keywords:
+    "Nano, Cryptocurrency, Faucet, Airdrop, Earn Free Nano, Crypto Community",
+  openGraph: {
+    title: "Nano Faucet - Earn Free Nano Cryptocurrency Effortlessly",
+    description:
+      "Join the Nano Faucet platform to earn free Nano cryptocurrency by referring friends and completing simple tasks. Claim your airdrop and be part of the growing Nano community today!",
+    url: "https://nanfaucet.com",
+    siteName: "Nano Faucet",
+    images: [
+      {
+        url: "https://nanfaucet.com/your-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Nano Faucet",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nano Faucet - Earn Free Nano Cryptocurrency Effortlessly",
+    description:
+      "Join the Nano Faucet platform to earn free Nano cryptocurrency by referring friends and completing simple tasks. Claim your airdrop and be part of the growing Nano community today!",
+    image: "https://nanfaucet.com/your-image.jpg",
+  },
 };
 
 // Trying to make sure the screen doesn't zoom in.
