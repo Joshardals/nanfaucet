ee3ecb271dcc66d2f494ce2936607803a609831e
Joshardals <joshuabamidele219@gmail.com>
2024-10-28 05:03:27 +0100

    initial commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nano Faucet - Earn Free Nano Cryptocurrency Effortlessly",
+  description:
+    "Join the Nano Faucet platform to earn free Nano cryptocurrency by referring friends. Claim your airdrop and be part of the growing Nano community today!",
+  keywords:
+    "Nano, Cryptocurrency, Faucet, Airdrop, Earn Free Nano, Crypto Community",
+  author: "NanoFaucet Team",
+};
+
 export default function Layout({
   children,
 }: Readonly<{
@@ -5,7 +16,9 @@ export default function Layout({
 }>) {
   return (
     <div className="bg-gray-200 flex items-center justify-center min-h-screen max-sm:p-5">
-      <div className="bg-white shadow-md p-5 rounded-xl w-full sm:w-96">{children}</div>
+      <div className="bg-white shadow-md p-5 rounded-xl w-full sm:w-96">
+        {children}
+      </div>
     </div>
   );
 }
